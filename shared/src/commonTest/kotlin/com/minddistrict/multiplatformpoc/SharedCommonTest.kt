c4a5e8edc5eb6cb1beeff2a2c96ccde0d339cd81
Borja Quevedo <b.quevedo@minddistrict.com>
2025-11-24 15:26:47 +0100

    ==build: add initial Kotlin Multiplatform project structure from wizard

---
+++ shared/src/commonTest/kotlin/com/minddistrict/multiplatformpoc/SharedCommonTest.kt
@@ -0,0 +1,12 @@
+package com.minddistrict.multiplatformpoc
+
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+class SharedCommonTest {
+
+    @Test
+    fun example() {
+        assertEquals(3, 1 + 2)
+    }
+}
