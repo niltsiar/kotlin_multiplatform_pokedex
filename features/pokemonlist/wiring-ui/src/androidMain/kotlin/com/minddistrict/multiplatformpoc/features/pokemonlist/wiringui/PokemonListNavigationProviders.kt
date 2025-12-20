d364e934852c462d4ab4a7b1fc145b9ab4a53fca
Borja Quevedo <b.quevedo@minddistrict.com>
2025-12-20 17:58:18 +0100

    feat(pokemonlist): move navigation providers to wiring-ui

---
+++ features/pokemonlist/wiring-ui/src/androidMain/kotlin/com/minddistrict/multiplatformpoc/features/pokemonlist/wiringui/PokemonListNavigationProviders.kt
@@ -0,0 +1,36 @@
+package com.minddistrict.multiplatformpoc.features.pokemonlist.wiringui
+
+import com.minddistrict.multiplatformpoc.core.navigation.EntryProviderInstaller
+import com.minddistrict.multiplatformpoc.core.navigation.Navigator
+import com.minddistrict.multiplatformpoc.features.pokemondetail.navigation.PokemonDetail
+import com.minddistrict.multiplatformpoc.features.pokemonlist.navigation.PokemonList
+import com.minddistrict.multiplatformpoc.features.pokemonlist.presentation.PokemonListViewModel
+import com.minddistrict.multiplatformpoc.features.pokemonlist.ui.PokemonListScreen
+import org.koin.compose.koinInject
+import org.koin.core.qualifier.named
+import org.koin.dsl.module
+
+/**
+ * Android-specific Koin DI module for Pokemon List navigation.
+ *
+ * Provides the EntryProviderInstaller that registers the PokemonListScreen composable with the navigation graph.
+ */
+val pokemonListNavigationModule = module {
+    single<Set<EntryProviderInstaller>>(named(PokemonListNavigationInstallersQualifier)) {
+        setOf(
+            {
+                entry<PokemonList> {
+                    val navigator: Navigator = koinInject()
+                    val viewModel: PokemonListViewModel = koinInject()
+
+                    PokemonListScreen(
+                        viewModel = viewModel,
+                        onPokemonClick = { pokemon ->
+                            navigator.goTo(PokemonDetail(pokemon.id))
+                        },
+                    )
+                }
+            },
+        )
+    }
+}
